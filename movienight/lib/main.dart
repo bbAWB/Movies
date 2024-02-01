31e3c8191abb5c4d755a48f7135581a63c139269
blenbetassefa <114851071+blenbetassefa@users.noreply.github.com>
2024-02-01 21:08:59 +0300

    first commit

---
+++ movienight/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Movie());
+}
+
+class Movie extends StatelessWidget {
+  const Movie({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
